5337cc8f4ccc4c55633ff11a7f2443ea6e15bf55
mohammadne <mohammadne.dev@gmail.com>
2023-04-19 09:28:41 +0330

    feat: add golang project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mohammadne/simple-ci
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const timeout = 3 * time.Second
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	server := &http.Server{
+		Addr:              ":8090",
+		ReadHeaderTimeout: timeout,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
+
+func hello(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "hello from devopshobbies\n")
+}
